68720a98abb15f43446b303990caf7d625bd70ac
ajospino <ajospinoa@eafit.edu.co>
2023-04-15 12:17:00 -0500

    Test1 para AWS

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -2,7 +2,7 @@ package co.edu.eafit.protobootapp;
 
 //This is a simple calculator class.
 public class Calculadora {
-
+    //Test changes on code
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
